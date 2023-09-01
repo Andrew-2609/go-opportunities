f4932c0bfed047e86b0d39d93507524b8925a7e7
Andrew Monteiro <andrew.agoravai@gmail.com>
2023-09-01 09:04:58 -0300

    feat: implement getOpenings handler logic

--- handler/getOpenings.go
+++ handler/getOpenings.go
@@ -3,11 +3,18 @@ package handler
 import (
 	"net/http"
 
+	"github.com/Andrew-2609/go-opportunities/schema"
 	"github.com/gin-gonic/gin"
 )
 
 func GetOpeningsHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "GET Openings",
-	})
+	openings := []schema.Opening{}
+
+	if err := db.Find(&openings).Error; err != nil {
+		logger.Errorf("Error retrieving all openings: %v", err.Error())
+		sendError(ctx, http.StatusInternalServerError, "An unexpected error occurred. Please try again later or contact the support team!")
+		return
+	}
+
+	sendOK(ctx, openings)
 }
