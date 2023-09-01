b32398d3ea30bb53efc2e2aae15aa868b4d4f5d8
Andrew Monteiro <andrew.agoravai@gmail.com>
2023-09-01 09:10:15 -0300

    feat: add getOpening handler logic

--- handler/getOpening.go
+++ handler/getOpening.go
@@ -1,13 +1,30 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
 
+	"github.com/Andrew-2609/go-opportunities/schema"
 	"github.com/gin-gonic/gin"
 )
 
 func GetOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "GET Opening",
-	})
+	id := ctx.Param("id")
+
+	validId, err := strconv.Atoi(id)
+
+	if err != nil {
+		sendError(ctx, http.StatusBadRequest, fmt.Sprintf("ID attribute must be of type int64. '%s' is not valid!", id))
+		return
+	}
+
+	opening := schema.Opening{}
+
+	if err := db.First(&opening, validId).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("There's no Opening with the id %d", validId))
+		return
+	}
+
+	sendOK(ctx, opening)
 }
