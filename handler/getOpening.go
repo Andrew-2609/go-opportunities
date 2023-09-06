8c0509f764a492c71d3114b6933220a43e559f99
Andrew Monteiro <andrew.agoravai@gmail.com>
2023-09-06 08:17:00 -0300

    docs: add docs for Get Opening route

--- handler/getOpening.go
+++ handler/getOpening.go
@@ -9,6 +9,16 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// @Summary Get Opening
+// @Description Get a job opening
+// @Tags Openings
+// @Produce json
+// @Param id path int true "Opening ID"
+// @Success 200 {object} schema.OpeningResponse
+// @Failure 400 {object} ErrorResponse
+// @Failure 404 {object} ErrorResponse
+// @Failure 500 {object} ErrorResponse
+// @Router /openings/{id} [get]
 func GetOpeningHandler(ctx *gin.Context) {
 	id := ctx.Param("id")
 
